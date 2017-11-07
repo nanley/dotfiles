b467a0cecb7d4e2fce2271128f53e98ec161042b
Nanley Chery <nanleychery@gmail.com>
2017-11-07 12:04:55 -0800

    xmonad: Make this branch rebase-friendly
    This master branch will be the base of different topic branches.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,69 +1,35 @@
-import XMonad
-import XMonad.Layout.Cross
-import XMonad.Prompt
-import XMonad.Prompt.Shell
-import XMonad.Util.EZConfig
-import Graphics.X11.ExtraTypes.XF86
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.UrgencyHook
 import Data.List
+import Graphics.X11.ExtraTypes.XF86
+import XMonad
+import XMonad.Util.EZConfig
 
-myLayout = tiled ||| Mirror split ||| simpleCross
-  where
-     -- Tall nmaster delta mratio
-     split   = Tall 2 (3/100) (9/10)
-     tiled   = Tall 1 (3/100) (1/2)
-
--- Start xmonad with xmobar and customizations.
-main = xmonad =<< statusBar "xmobar" bwBarPP toggleStrutsKey (withUrgencyHook NoUrgencyHook
-  $ def {
-    -- [1..4] : work items, increasing importance from right to left  (L<-R)
-    -- "-"    : long-running rarely-checked windows
-    -- [6..9] : other items, increasing importance from left to right (L->R)
-      workspaces         = map show [1..4] ++ ("-" : map show [6..9])
-
+main = xmonad
+  (def {
     -- Change the default terminal for: window resizing, clickable
     -- URLs, and urgent hints.
-    , terminal           = "terminator"
-    , layoutHook         = myLayout
-
-    -- Workaround a bug with shellPrompt. When opening some applications
-    -- (like gnome-dictionary) and the mouse is on any window,
-    -- the window opens above the mouse's window and not the currently focused one.
-    , focusFollowsMouse  = False
+      terminal   = "terminator"
+    , layoutHook = myLayout
   } `additionalKeys` [
-      ((mod1Mask,        xK_p                    ), shellPrompt bwPrompt)
+      ((mod4Mask, xK_l                    ), spawn "i3lock")
 
-    -- Fancy Keys
-    , ((0,               xF86XK_AudioRaiseVolume ), spawn $ setVolFeedback "+5%")
-    , ((0,               xF86XK_AudioLowerVolume ), spawn $ setVolFeedback "-5%")
-    , ((0,               xF86XK_AudioMute        ), spawn $ setMute True)
-    , ((0,               xF86XK_MonBrightnessUp  ), spawn "xbacklight +5%")
-    , ((0,               xF86XK_MonBrightnessDown), spawn "xbacklight -5%")
-
-    , ((mod4Mask,        xK_l                    ), spawn "i3lock")
+    -- Audio
+    , ((0,        xF86XK_AudioRaiseVolume ), spawn $ setVolFeedback "+5%")
+    , ((0,        xF86XK_AudioLowerVolume ), spawn $ setVolFeedback "-5%")
+    , ((0,        xF86XK_AudioMute        ), spawn $ setMute True)
 
     -- Appearance
-    , ((mod4Mask,        xK_i                    ), spawn "xrandr-invert-colors")
-    , ((mod4Mask,        xK_r                    ), spawn "redshift -O 3500")
-    , ((mod4Mask,        xK_b                    ), spawn "redshift -O 5500")
+    , ((0,        xF86XK_MonBrightnessUp  ), spawn "xbacklight +5%")
+    , ((0,        xF86XK_MonBrightnessDown), spawn "xbacklight -5%")
+    , ((mod4Mask, xK_i                    ), spawn "xrandr-invert-colors")
+    , ((mod4Mask, xK_r                    ), spawn "redshift -O 3500")
+    , ((mod4Mask, xK_b                    ), spawn "redshift -O 5500")
   ])
 
--- A simple black and white color scheme for log info
-bwBarPP = def {ppUrgent  = xmobarColor "white" "black" . pad}
-
--- Add the shift mask to the default keybinding to avoid a conflict with our terminal
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask .|. shiftMask, xK_b)
-
--- A prompt that takes command options
-bwPrompt = def {  font ="xft:Mono:size=10"
-                  , fgColor = "black"
-                  , bgColor = "white"
-                  , borderColor = "red"
-                  , position = Top
-                  , alwaysHighlight = True
-                  , height = 20
-                 }
+myLayout = tiled ||| Mirror split ||| Full
+  where
+     -- Tall nmaster delta mratio
+     split = Tall 2 (3/100) (9/10)
+     tiled = Tall 1 (3/100) (1/2)
 
 setVolUnmute :: String -> String
 setVolUnmute x = mergeCmd [setMute False, setVol x]
