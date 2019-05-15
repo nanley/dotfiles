9f32ec9bda875646f1d39cc6e25924aff12de891
Nanley Chery <nanleychery@gmail.com>
2019-05-14 22:38:36 -0700

    nixos: Add ctags

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -24,7 +24,7 @@
   environment.systemPackages = with pkgs; [
 
     # Console apps
-    tmux htop stow git cscope tig udisks
+    tmux htop stow git cscope ctags tig udisks
 
     # Graphical apps
     evince scrot
